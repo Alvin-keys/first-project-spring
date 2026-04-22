e9fea3f7ab4a8eca5411a2607a64502c3cf50deb
Alvino Junior <alvinojrdev@gmail.com>
2026-04-22 20:26:14 -0300

    first commit

---
+++ src/main/java/com/educandoweb/coursex/CoursexApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursexApplication.class, args);
+	}
+
+}
